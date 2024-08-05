19ff8b70eff05d0f0fe48a319f281e6111a46ac2
Rigel Ramadhan <rigelramadhan@gmail.com>
2024-08-05 16:22:04 +0700

    🤙 Setup app theme

---
+++ feature/consult/src/main/java/one/reevdev/medosense/feature/consult/screen/illness/input/IllnessInputRouter.kt
@@ -0,0 +1,9 @@
+package one.reevdev.medosense.feature.consult.screen.illness.input
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun IllnessInputRouter(modifier: Modifier = Modifier) {
+
+}
---
+++ feature/consult/src/main/java/one/reevdev/medosense/feature/consult/screen/illness/input/IllnessInputScreen.kt
@@ -0,0 +1,23 @@
+package one.reevdev.medosense.feature.consult.screen.illness.input
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import one.reevdev.medosense.feature.common.theme.MedosenseTheme
+
+@Composable
+fun IllnessInputScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Preview
+@Composable
+private fun IllnessInputScreenPreview() {
+    MedosenseTheme {
+        IllnessInputScreen(
+
+        )
+    }
+}
